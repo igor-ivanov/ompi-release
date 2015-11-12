igor-ivanov/ompi-release

commit 74e1de7835de0f538c7ebb669c5dc1732c6ec923

OSHMEM/SPML/ikrit: cleanup

oshmem/mca/spml/ikrit/spml_ikrit.c
@@ -38,6 +38,19 @@
 #define SPML_IKRIT_PUT_DEBUG    0
 #endif
 
+#define SPML_IKRIT_MXM_POST_SEND(sreq) \
+do { \
+    mxm_error_t err; \
+    err = mxm_req_send(&sreq); \
+    if (MXM_OK != err) { \
+        SPML_ERROR("mxm_req_send (op=%d) failed: %s - aborting", \
+                   sreq.opcode, \
+                   mxm_error_string(err)); \
+        oshmem_shmem_abort(-1); \
+        return OSHMEM_ERROR; \
+    } \
+} while(0)
+
 typedef struct spml_ikrit_am_hdr {
     uint64_t va;
 } spml_ikrit_am_hdr_t;
@@ -85,6 +98,7 @@ static inline mxm_mem_key_t *to_mxm_mkey(sshmem_mkey_t *mkey) {
 }
 #endif
 
+
 static inline void mca_spml_irkit_req_wait(mxm_req_base_t *req)
 {
     while (!mxm_req_test(req))
@@ -96,8 +110,8 @@ static int mca_spml_ikrit_put_request_free(struct oshmem_request_t** request)
     mca_spml_ikrit_put_request_t *put_req =
             *(mca_spml_ikrit_put_request_t **) request;
 
-    assert(false == put_req->req_put.req_base.req_free_called);
     OPAL_THREAD_LOCK(&oshmem_request_lock);
+    assert(false == put_req->req_put.req_base.req_free_called);
     put_req->req_put.req_base.req_free_called = true;
     OMPI_FREE_LIST_RETURN_MT( &mca_spml_base_put_requests,
                           (ompi_free_list_item_t*)put_req);
@@ -144,8 +158,8 @@ static int mca_spml_ikrit_get_request_free(struct oshmem_request_t** request)
     mca_spml_ikrit_get_request_t *get_req =
             *(mca_spml_ikrit_get_request_t **) request;
 
-    assert(false == get_req->req_get.req_base.req_free_called);
     OPAL_THREAD_LOCK(&oshmem_request_lock);
+    assert(false == get_req->req_get.req_base.req_free_called);
     get_req->req_get.req_base.req_free_called = true;
     OMPI_FREE_LIST_RETURN_MT( &mca_spml_base_get_requests,
                           (ompi_free_list_item_t*)get_req);
@@ -164,7 +178,7 @@ static int mca_spml_ikrit_get_request_cancel(struct oshmem_request_t * request,
 
 static void mca_spml_ikrit_get_request_construct(mca_spml_ikrit_get_request_t* req)
 {
-    req->req_get.req_base.req_type = MCA_SPML_REQUEST_PUT;
+    req->req_get.req_base.req_type = MCA_SPML_REQUEST_GET;
     req->req_get.req_base.req_oshmem.req_free = mca_spml_ikrit_get_request_free;
     req->req_get.req_base.req_oshmem.req_cancel =
             mca_spml_ikrit_get_request_cancel;
@@ -357,19 +371,15 @@ int mca_spml_ikrit_del_procs(oshmem_proc_t** procs, size_t nprocs)
 
     for (n = 0; n < nprocs; n++) {
         i = (my_rank + n) % nprocs;
-        if (mca_spml_ikrit.mxm_peers[i]->mxm_conn) {
-            mxm_ep_disconnect(mca_spml_ikrit.mxm_peers[i]->mxm_conn);
-        }
-        if (mca_spml_ikrit.hw_rdma_channel && mca_spml_ikrit.mxm_peers[i]->mxm_hw_rdma_conn) {
+        mxm_ep_disconnect(mca_spml_ikrit.mxm_peers[i]->mxm_conn);
+        if (mca_spml_ikrit.hw_rdma_channel) {
+            assert(mca_spml_ikrit.mxm_peers[i]->mxm_hw_rdma_conn != mca_spml_ikrit.mxm_peers[i]->mxm_conn);
             mxm_ep_disconnect(mca_spml_ikrit.mxm_peers[i]->mxm_hw_rdma_conn);
         }
         destroy_ptl_idx(i);
-        if (mca_spml_ikrit.mxm_peers[i]) {
-            OBJ_RELEASE(mca_spml_ikrit.mxm_peers[i]);
-        }
+        OBJ_RELEASE(mca_spml_ikrit.mxm_peers[i]);
     }
-    if (mca_spml_ikrit.mxm_peers)
-        free(mca_spml_ikrit.mxm_peers);
+    free(mca_spml_ikrit.mxm_peers);
 
     return OSHMEM_SUCCESS;
 }
@@ -401,20 +411,18 @@ int mca_spml_ikrit_add_procs(oshmem_proc_t** procs, size_t nprocs)
     }
     memset(conn_reqs, 0x0, sizeof(mxm_conn_req_t));
 #endif
-    ep_info = malloc(nprocs * sizeof(spml_ikrit_mxm_ep_conn_info_t));
+    ep_info = calloc(sizeof(spml_ikrit_mxm_ep_conn_info_t), nprocs);
     if (NULL == ep_info) {
         rc = OSHMEM_ERR_OUT_OF_RESOURCE;
         goto bail;
     }
-    memset(ep_info, 0x0, sizeof(spml_ikrit_mxm_ep_conn_info_t));
 
     if (mca_spml_ikrit.hw_rdma_channel) {
-        ep_hw_rdma_info = malloc(nprocs * sizeof(spml_ikrit_mxm_ep_conn_info_t));
+        ep_hw_rdma_info = calloc(sizeof(spml_ikrit_mxm_ep_conn_info_t), nprocs);
         if (NULL == ep_hw_rdma_info) {
             rc = OSHMEM_ERR_OUT_OF_RESOURCE;
             goto bail;
         }
-        memset(ep_hw_rdma_info, 0x0, sizeof(spml_ikrit_mxm_ep_conn_info_t));
     }
 
     mca_spml_ikrit.mxm_peers = (mxm_peer_t **) malloc(nprocs
@@ -523,8 +531,10 @@ int mca_spml_ikrit_add_procs(oshmem_proc_t** procs, size_t nprocs)
     /* Save returned connections */
     for (i = 0; i < nprocs; ++i) {
         mca_spml_ikrit.mxm_peers[i]->mxm_conn = conn_reqs[i].conn;
-        if (OSHMEM_SUCCESS != create_ptl_idx(i))
+        if (OSHMEM_SUCCESS != create_ptl_idx(i)) {
+            rc = OSHMEM_ERR_CONNECTION_FAILED;
             goto bail;
+        }
 
         mxm_conn_ctx_set(conn_reqs[i].conn, mca_spml_ikrit.mxm_peers[i]);
     }
@@ -550,7 +560,7 @@ int mca_spml_ikrit_add_procs(oshmem_proc_t** procs, size_t nprocs)
             continue;
         }
         if (procs[i] == proc_self)
-        continue;
+            continue;
 
         /* use zcopy for put/get via sysv shared memory */
         procs[i]->transport_ids[0] = MXM_PTL_SHM;
@@ -838,6 +848,7 @@ static inline int mca_spml_ikrit_get_shm(void *src_addr,
 int mca_spml_ikrit_get(void *src_addr, size_t size, void *dst_addr, int src)
 {
     mxm_send_req_t sreq;
+    mxm_error_t err;
 
     if (0 >= size) {
         return OSHMEM_SUCCESS;
@@ -863,10 +874,9 @@ int mca_spml_ikrit_get(void *src_addr, size_t size, void *dst_addr, int src)
 #endif
     sreq.base.completed_cb = NULL;
 
-    mxm_req_send(&sreq);
-    opal_progress();
-    mca_spml_irkit_req_wait(&sreq.base);
+    SPML_IKRIT_MXM_POST_SEND(sreq);
 
+    mca_spml_irkit_req_wait(&sreq.base);
     if (MXM_OK != sreq.base.error) {
         SPML_ERROR("get request failed: %s - aborting",
                    mxm_error_string(sreq.base.error));
@@ -925,14 +935,8 @@ int mca_spml_ikrit_get_async(void *src_addr,
     get_req->mxm_req.base.context = get_req;
     OPAL_THREAD_ADD32(&mca_spml_ikrit.n_active_gets, 1);
 
-    mxm_req_send(&get_req->mxm_req);
+    SPML_IKRIT_MXM_POST_SEND(get_req->mxm_req);
 
-    if (MXM_OK != get_req->mxm_req.base.error) {
-        SPML_ERROR("get request failed: %s - aborting",
-                   mxm_error_string(get_req->mxm_req.base.error));
-        oshmem_shmem_abort(-1);
-        return OSHMEM_ERROR;
-    }
     return OSHMEM_SUCCESS;
 }
 
@@ -979,7 +983,7 @@ static int mca_spml_ikrit_mxm_fence(int dst)
     fence_req->mxm_req.base.context = fence_req;
     OPAL_THREAD_ADD32(&mca_spml_ikrit.n_mxm_fences, 1);
 
-    mxm_req_send(&fence_req->mxm_req);
+    SPML_IKRIT_MXM_POST_SEND(fence_req->mxm_req);
     return OSHMEM_SUCCESS;
 }
 
@@ -1158,15 +1162,8 @@ static inline int mca_spml_ikrit_put_internal(void* dst_addr,
 
     mca_spml_ikrit.mxm_peers[dst]->n_active_puts++;
 
-    mxm_req_send(&put_req->mxm_req);
+    SPML_IKRIT_MXM_POST_SEND(put_req->mxm_req);
 
-    if (MXM_OK != put_req->mxm_req.base.error) {
-        OPAL_THREAD_ADD32(&mca_spml_ikrit.n_active_puts, -1);
-        SPML_ERROR("put request %p failed: %s - aborting",
-                   (void*)put_req, mxm_error_string(put_req->mxm_req.base.error));
-        oshmem_shmem_abort(-1);
-        return OSHMEM_ERROR;
-    }
     if (need_progress)
         mxm_progress(mca_spml_ikrit.mxm_context);
 
@@ -1269,13 +1266,7 @@ int mca_spml_ikrit_put_simple(void* dst_addr,
         mca_spml_ikrit.mxm_peers[dst]->need_fence = 1;
     }
 
-    mxm_req_send(&mxm_req);
-    if (MXM_OK != mxm_req.base.error) {
-        SPML_ERROR("put request failed: %s(%d) - aborting",
-                   mxm_error_string(mxm_req.base.error), mxm_req.base.error);
-        oshmem_shmem_abort(-1);
-        return OSHMEM_ERROR;
-    }
+    SPML_IKRIT_MXM_POST_SEND(mxm_req);
 
     wait.req = &mxm_req.base;
     wait.state = (mxm_req_state_t)(MXM_REQ_SENT | MXM_REQ_COMPLETED);
@@ -1432,7 +1423,8 @@ int mca_spml_ikrit_send(void* buf,
     req.base.data.buffer.length = size == 0 ? sizeof(dummy_buf) : size;
     req.base.data.buffer.memh = NULL;
 
-    mxm_req_send(&req);
+    SPML_IKRIT_MXM_POST_SEND(req);
+
     mca_spml_irkit_req_wait(&req.base);
     if (req.base.error != MXM_OK) {
         return OSHMEM_ERROR;

oshmem/mca/spml/ikrit/spml_ikrit.h
@@ -163,15 +163,6 @@ extern int mca_spml_ikrit_del_procs(oshmem_proc_t** procs, size_t nprocs);
 extern int mca_spml_ikrit_fence(void);
 extern int spml_ikrit_progress(void);
 
-static inline oshmem_proc_t *mca_spml_ikrit_proc_find(int dst)
-{
-    orte_process_name_t name;
-
-    name.jobid = ORTE_PROC_MY_NAME->jobid;
-    name.vpid = dst;
-    return oshmem_proc_find(&name);
-}
-
 END_C_DECLS
 
 #endif

oshmem/mca/spml/ikrit/spml_ikrit_component.c
@@ -108,7 +108,7 @@ static inline int set_mxm_tls()
     tls = getenv("MXM_TLS");
     if (NULL == tls) {
         opal_setenv("MXM_OSHMEM_TLS", mca_spml_ikrit.mxm_tls, 1, &environ);
-        return OSHMEM_SUCCESS;
+        return check_mxm_tls("MXM_OSHMEM_TLS");
     }
     if (OSHMEM_SUCCESS == check_mxm_tls("MXM_TLS")) {
         opal_setenv("MXM_OSHMEM_TLS", tls, 1, &environ);
@@ -120,12 +120,14 @@ static inline int set_mxm_tls()
 static inline int check_mxm_hw_tls(char *v, char *tls)
 {
     if ((0 == strcmp(tls, "rc") || 0 == strcmp(tls, "dc"))) {
+        mca_spml_ikrit.ud_only = 0;
         return OSHMEM_SUCCESS;
     }
 
     if (strstr(tls, "ud") &&
             (NULL == strstr(tls, "rc") && NULL == strstr(tls, "dc") &&
              NULL == strstr(tls, "shm"))) {
+        mca_spml_ikrit.ud_only = 1;
         return OSHMEM_SUCCESS;
     }
 
@@ -141,6 +143,8 @@ static inline int set_mxm_hw_rdma_tls()
     }
     opal_setenv("MXM_OSHMEM_HW_RDMA_RC_QP_LIMIT", "-1", 0, &environ);
     opal_setenv("MXM_OSHMEM_HW_RDMA_TLS", "rc", 0, &environ);
+    SPML_VERBOSE(5, "Additional communication channel is enabled. Transports are: %s",
+            getenv("MXM_OSHMEM_HW_RDMA_TLS"));
 
     return check_mxm_hw_tls("MXM_OSHMEM_HW_RDMA_TLS", 
             getenv("MXM_OSHMEM_HW_RDMA_TLS"));
@@ -295,6 +299,8 @@ static int mca_spml_ikrit_component_open(void)
     mca_spml_ikrit.ud_only = 1;
     mca_spml_ikrit.mxm_ctx_opts->ptl_bitmap = (MXM_BIT(MXM_PTL_SELF) | MXM_BIT(MXM_PTL_RDMA));
 #endif
+    SPML_VERBOSE(5, "UD only mode is %s",
+                 mca_spml_ikrit.ud_only ? "enabled" : "disabled");
 
     err = mxm_init(mca_spml_ikrit.mxm_ctx_opts, &mca_spml_ikrit.mxm_context);
     if (MXM_OK != err) {

commit 2f6f762ba5a709c5a66058775ac1a8e892f68dc0

SPML/IKRIT: make sure that opal_progress is called at least once
Some MXM tls such as self, shm can comlete requests immediately.
Make sure that opal_progress() is called before before request
is completed.

Fixes the problem introduced in 74e1de7

oshmem/mca/spml/ikrit/spml_ikrit.c
@@ -102,8 +102,13 @@ static inline mxm_mem_key_t *to_mxm_mkey(sshmem_mkey_t *mkey) {
 
 static inline void mca_spml_irkit_req_wait(mxm_req_base_t *req)
 {
-    while (!mxm_req_test(req))
+    do {
+        /* do at least one progress since
+         * with some TLs (self, shm) request
+         * can be completed immediately
+         */
         opal_progress();
+    } while (!mxm_req_test(req));
 }
 
 static int mca_spml_ikrit_put_request_free(struct oshmem_request_t** request)